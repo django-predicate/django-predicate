f5844e3c5c061db8f028d2d23702a9777b184392
Lucas Wiman <lucaswiman@counsyl.com>
2015-12-14 23:10:57 -0800

    Add tox.ini file to cover Django 1.7 and 1.8.
    The test suite fails on Django 1.9.
    
    The tox configuration also generates an HTML coverage report.
    
    Remove extraneous testproject files

---
+++ runtests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "tests.testapp.settings")
+
+from django.conf import settings
+from django_nose import NoseTestSuiteRunner
+
+
+def runtests(*test_args, **kwargs):
+    if not test_args:
+        test_args = ['tests']
+    # Do not prompt to destroy existing db
+    kwargs.setdefault('interactive', False)
+
+    test_runner = NoseTestSuiteRunner(**kwargs)
+
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests()
