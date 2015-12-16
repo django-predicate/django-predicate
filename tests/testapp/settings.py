b9d1ae89310208ba8016017aa500b02d6abac89e
Lucas Wiman <lucas.wiman@gmail.com>
2015-12-15 22:05:27 -0800

    Add tox targets for postgres

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -1,4 +1,4 @@
-# Django settings for testapp project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,16 +9,28 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+_BACKEND = os.environ.get("DB_BACKEND", "sqlite3")
+
+if _BACKEND == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test.db',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
     }
-}
+elif _BACKEND == 'postgresql_psycopg2':
+    DATABASES = {
+        'default': {
+            'NAME': 'predicatedb',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'USER': 'django',
+            'PASSWORD': 'secret',
+        },
+    }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
